b28f89081b491b8c6bcdd7b0a94a0601447ee230
tristan <tdbrester@gmail.com>
2024-10-31 14:36:30 -0700

    lesson

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,5 +1,6 @@
 package lesson1;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         guessMyNumber();
@@ -7,6 +8,29 @@ public class Main {
     }
 
     public static void guessMyNumber(){
+        //scanner things
+        Scanner sc = new Scanner(System.in);
 
+        //make random number
+        Random rand = new Random();
+        int randNumber = rand.nextInt(10);
+
+        //start the loop
+        System.out.println("See if you can guess my number!");
+
+        boolean guessed = false;
+        do {
+            System.out.print("Enter a number:\n>");
+            int guess = sc.nextInt();
+
+            if(guess == randNumber){
+                System.out.println("Correct!");
+                guessed = true;
+            }
+            else{
+                System.out.println("Try Again!");
+            }
+        }
+        while(!guessed);
     }
 }

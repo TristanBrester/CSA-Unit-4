cc0f336579a8855128285cb371d8de3c2a0886d9
tristan <tdbrester@gmail.com>
2024-11-18 15:03:49 -0800

    homework

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -25,10 +25,60 @@ public class Main {
     }
 
     public static void vowelCounter(String str) {
+        int aCount = 0;
+        int eCount = 0;
+        int iCount = 0;
+        int oCount = 0;
+        int uCount = 0;
 
+        for(int i = 0; i < str.length(); i++) {
+            switch(str.charAt(i)) {
+                case 'a':
+                    aCount++;
+                    break;
+                case 'e':
+                    eCount++;
+                    break;
+                case 'i':
+                    iCount++;
+                    break;
+                case 'o':
+                    oCount++;
+                    break;
+                case 'u':
+                    uCount++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.printf("""
+                Number of a's: %d
+                Number of e's: %d
+                Number of i's: %d
+                Number of o's: %d
+                Number of u's: %d
+                """,aCount,eCount,iCount,oCount,uCount);
     }
 
     public static void palindromeChecker(String str) {
-
+        boolean palindrome = true;
+        str = str.toLowerCase();
+        for(int i = str.length() - 1; i >= 0; i--) {
+            Character oppositeLetter = str.charAt(Math.abs(str.length() - i)-1);
+            if(!Character.isLetter(str.charAt(i)) || !Character.isLetter(oppositeLetter)) {
+                System.out.printf("Error: Invalid character detected in %s",str);
+                palindrome = false;
+                break;
+            }
+            if(str.charAt(i) != oppositeLetter) {
+                System.out.printf("%s is not a palindrome",str);
+                palindrome = false;
+                break;
+            }
+        }
+        if(palindrome) {
+            System.out.printf("%s is a palindrome",str);
+        }
     }
 }
